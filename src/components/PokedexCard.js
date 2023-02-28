cc88fd89a516fccde5c1a2ee8d5e5b82e9d02c46
Anna Baker <anna.baker.design@gmail.com>
2023-02-28 13:58:58 -0500

    PokedexCard data

--- src/components/PokedexCard.js
+++ src/components/PokedexCard.js
@@ -1,4 +1,4 @@
-import { Card, CardActionArea, CardContent, Link, Skeleton } from "@mui/material";
+import { Card, CardActionArea, CardContent, Chip, Link, Skeleton, Typography } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2';
 import { formatDexId } from "../utilities/utilities";
 
@@ -23,14 +23,46 @@ const PokedexCard = (props) => {
 										alt={p.name}
 									/>
 								</Grid>
-								<Grid xs={7}>
-									<h2>
-										<span style={{ fontSize: "0.5em", marginRight: 4 }}>
-											<span style={{ fontSize: "0.7em", marginRight: 1 }}>No.</span>
-											{formatDexId(p.id)}
-										</span>
-										{p.name}
-									</h2>
+								<Grid container xs={7}>
+									<Grid xs={12}>
+										<Typography variant="h6" component="h2" sx={{ textTransform: "capitalize" }}>
+											<Typography component="span" sx={{ fontSize: "0.75em", marginRight: 4 }}>
+												<Typography component="span" sx={{ fontSize: "0.75em", marginRight: 1 }}>No.</Typography>
+												{formatDexId(p.id)}
+											</Typography>
+											{p.name}
+										</Typography>
+									</Grid>
+									<Grid container xs={12}>
+										<Chip label={p.types[0].type.name} size="small" />
+										{p.types[1] ? <Chip label={p.types[1].type.name} size="small" /> : null}
+									</Grid>
+									<Grid container xs={12}>
+										<Grid xs>
+											<Typography variant="caption" component="h3">HP</Typography>
+											<Typography variant="caption" component="p">{p.stats[0].base_stat}</Typography>
+										</Grid>
+										<Grid xs>
+											<Typography variant="caption" component="h3">ATT</Typography>
+											<Typography variant="caption" component="p">{p.stats[1].base_stat}</Typography>
+										</Grid>
+										<Grid xs>
+											<Typography variant="caption" component="h3">DEF</Typography>
+											<Typography variant="caption" component="p">{p.stats[2].base_stat}</Typography>
+										</Grid>
+										<Grid xs>
+											<Typography variant="caption" component="h3">SP.ATT</Typography>
+											<Typography variant="caption" component="p">{p.stats[3].base_stat}</Typography>
+										</Grid>
+										<Grid xs>
+											<Typography variant="caption" component="h3">SP.DEF</Typography>
+											<Typography variant="caption" component="p">{p.stats[4].base_stat}</Typography>
+										</Grid>
+										<Grid xs>
+											<Typography variant="caption" component="h3">SPD</Typography>
+											<Typography variant="caption" component="p">{p.stats[5].base_stat}</Typography>
+										</Grid>
+									</Grid>
 								</Grid>
 							</Grid>
 						</CardContent>
