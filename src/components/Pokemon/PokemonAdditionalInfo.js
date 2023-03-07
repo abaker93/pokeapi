d228a0d62f08783ff2df41372a88107b6d1e9ad9
Anna Baker <anna.baker.design@gmail.com>
2023-03-07 14:14:09 -0500

    height & weight info

--- src/components/Pokemon/PokemonAdditionalInfo.js
+++ src/components/Pokemon/PokemonAdditionalInfo.js
@@ -1,5 +1,6 @@
 import { Container, Typography } from "@mui/material";
 import { FemaleSharp, MaleSharp } from "@mui/icons-material";
+import { calcMeasurement } from "../../utilities/utilities";
 
 export default function PokemonAdditionalInfo (props) {
 	const p = props[0];
@@ -15,10 +16,10 @@ export default function PokemonAdditionalInfo (props) {
 			{!genderless ? (
 				<div className="col gender grid">
 					<Typography variant="h3">Gender</Typography>
-					<div class="col">
+					<div className="col">
 						<div className="pie-chart" style={{ "--male-percentage": male * 100 }}></div>
 					</div>
-					<div class="col">
+					<div className="col">
 						<Typography variant="body1"><FemaleSharp sx={{ fontSize: '1em' }} /> {female * 100}%</Typography>
 						<Typography variant="body1"><MaleSharp sx={{ fontSize: '1em' }} /> {male * 100}%</Typography>
 					</div>
@@ -28,6 +29,18 @@ export default function PokemonAdditionalInfo (props) {
 					<Typography variant="body1">Genderless</Typography>
 				</div>
 			)}
+
+			<div className="col">
+				<Typography variant="h3">Height</Typography>
+				<Typography variant="body1">{calcMeasurement(p.height, "in")}</Typography>
+				<Typography variant="body1">{calcMeasurement(p.height, "cm")}</Typography>
+			</div>
+
+			<div className="col">
+				<Typography variant="h3">Weight</Typography>
+				<Typography variant="body1">{calcMeasurement(p.weight, "lbs")}</Typography>
+				<Typography variant="body1">{calcMeasurement(p.weight, "kg")}</Typography>
+			</div>
 		</Container>
 	)
 }
--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -2,27 +2,6 @@ import { defense } from "./data";
 
 export const baseURL = 'https://pokeapi.co/api/v2';
 
-export const capitalize = text => {
-	return text.toLowerCase().replace(/\b(\w)/g, x => x.toUpperCase());
-}
-
-export const formatDexId = num => {
-	return num < 10
-		?	'000' + num.toString()
-		: num < 100
-			? '00' + num.toString()
-			: num < 1000
-				? '0' + num.toString()
-				: num.toString();
-}
-
-export const normalize = (value) => {
-	const min = 1;
-	const max = 255;
-
-	return ((value - min) * 100) / (max - min)
-};
-
 export const calcDefense = (def1, def2, off) => {
 	let calc;
 	let val;
@@ -40,4 +19,59 @@ export const calcDefense = (def1, def2, off) => {
 			: val = calc;
 
 	return (val)
+};
+
+export const calcMeasurement = (num, type) => {
+	let calc;
+
+	switch (type) {
+		case "in": {
+			let meas = round2Decimal(num * 3.93701);
+			let feet = Math.floor(meas / 12);
+			let inches = Math.round(meas % 12);
+			inches < 10 ? inches = '0' + inches.toString() : inches = inches.toString();
+
+			calc = (`${feet}' ${inches}"`)
+
+			break;
+		}
+		case "cm":
+			calc = (`${round2Decimal(num * 10)} cm`);
+			break;
+		case "lbs":
+			calc = (`${round2Decimal(num * 0.220462)} lbs`);
+			break;
+		case "kg":
+			calc = (`${round2Decimal(num / 10)} kg`);
+			break;
+		default:
+			console.log(`${type} is not a valid measurement type`);
+	}
+
+	return calc;
 }
+
+export const capitalize = text => {
+	return text.toLowerCase().replace(/\b(\w)/g, x => x.toUpperCase());
+};
+
+export const formatDexId = num => {
+	return num < 10
+		?	'000' + num.toString()
+		: num < 100
+			? '00' + num.toString()
+			: num < 1000
+				? '0' + num.toString()
+				: num.toString();
+};
+
+export const normalize = (value) => {
+	const min = 1;
+	const max = 255;
+
+	return ((value - min) * 100) / (max - min)
+};
+
+export const round2Decimal = num => {
+	return Math.round(num * 100) / 100;
+};
