4172865716dc690d9147f87230a58ed7ef0373fc
Anna Baker <anna.baker.design@gmail.com>
2023-03-14 17:41:20 -0400

    restructured PokemonAdditionalInfo.js

--- src/components/Pokemon/PokemonAdditionalInfo.js
+++ src/components/Pokemon/PokemonAdditionalInfo.js
@@ -1,4 +1,5 @@
 import { Chip, Container, Typography } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
 import { FemaleSharp, MaleSharp } from "@mui/icons-material";
 import { calcMeasurement } from "../../utilities/utilities";
 
@@ -11,77 +12,79 @@ export default function PokemonAdditionalInfo (props) {
 
 	return (
 		<Container id="PokemonAdditionalInfo">
-			<Typography variant="h2">Additional Info</Typography>
+			<Typography variant="h2" mb={1}>Additional Info</Typography>
 
-			<div className="row grid">
-				{!genderless ? (
-					<div className="col gender grid">
+			<Grid container mb={5}>
+				<Grid container xs={6}>
+					<Grid xs={12} mb={1}>
 						<Typography variant="h3">Gender</Typography>
-						<div className="col">
-							<div className="pie-chart" style={{ "--male-percentage": male * 100 }}></div>
-						</div>
-						<div className="col">
-							<Typography variant="body1"><FemaleSharp sx={{ fontSize: '1em' }} /> {female * 100}%</Typography>
-							<Typography variant="body1"><MaleSharp sx={{ fontSize: '1em' }} /> {male * 100}%</Typography>
-						</div>
-					</div>
-				) : (
-					<div className="col">
-						<Typography variant="body1">Genderless</Typography>
-					</div>
-				)}
+					</Grid>
+					{!genderless ? (
+						<>
+							<Grid xs={6}>
+								<div className="pie-chart" style={{ "--male-percentage": male * 100 }}></div>
+							</Grid>
+							<Grid xs={6}>
+								<Typography variant="body1"><FemaleSharp sx={{ fontSize: '1em' }} /> {female * 100}%</Typography>
+								<Typography variant="body1"><MaleSharp sx={{ fontSize: '1em' }} /> {male * 100}%</Typography>
+							</Grid>
+						</>
+					) : (
+						<Grid xs={12}>
+							<Typography variant="body1">Genderless</Typography>
+						</Grid>
+					)}
+				</Grid>
 
-				<div className="col">
-					<Typography variant="h3">Height</Typography>
+				<Grid xs={3}>
+					<Typography variant="h3" mb={1}>Height</Typography>
 					<Typography variant="body1">{calcMeasurement(p.height, "in")}</Typography>
 					<Typography variant="body1">{calcMeasurement(p.height, "cm")}</Typography>
-				</div>
+				</Grid>
 
-				<div className="col">
-					<Typography variant="h3">Weight</Typography>
+				<div xs={3}>
+					<Typography variant="h3" mb={1}>Weight</Typography>
 					<Typography variant="body1">{calcMeasurement(p.weight, "lbs")}</Typography>
 					<Typography variant="body1">{calcMeasurement(p.weight, "kg")}</Typography>
 				</div>
-			</div>
+			</Grid>
 
-			<div className="row">
-				<div className="col">
-					<Typography variant="h3">EV Yield</Typography>
+			<Grid container mb={5}>
+				<Grid xs>
+					<Typography variant="h3" mb={1}>EV Yield</Typography>
 					{p.stats.filter(f => f.effort > 0).map(m => (
 						<Chip size="small" label={`${m.effort} ${m.stat.name}`} />
 					))}
-				</div>
-			</div>
+				</Grid>
+			</Grid>
 
-			<div className="row">
-				<div className="col">
+			<Grid container mb={5} rowSpacing={1} columnSpacing={2}>
+				<Grid xs={7}>
 					<Typography component="h4" variant="h6">Catch Rate</Typography>
-				</div>
-				<div className="col">
+				</Grid>
+				<Grid xs={5}>
 					{/* // TODO: need to figure out math for capture rate with a reg pokebal, full hp */}
 					<Typography variant="body1">{p.capture_rate}</Typography>
-				</div>
-				<div className="col">
+				</Grid>
+				<Grid xs={7}>
 					<Typography component="h4" variant="h6">Base Friendship</Typography>
-				</div>
-				<div className="col">
+				</Grid>
+				<Grid xs={5}>
 					<Typography variant="body1"></Typography>
-				</div>
-				<div className="col">
+				</Grid>
+				<Grid xs={7}>
 					<Typography component="h4" variant="h6">Base Experience</Typography>
-				</div>
-				<div className="col">
-					{/* // TODO: need to figure out math for capture rate with a reg pokebal, full hp */}
+				</Grid>
+				<Grid xs={5}>
 					<Typography variant="body1">{p.base_experience}</Typography>
-				</div>
-				<div className="col">
+				</Grid>
+				<Grid xs={7}>
 					<Typography component="h4" variant="h6">Growth Rate</Typography>
-				</div>
-				<div className="col">
-					{/* // TODO: need to figure out math for capture rate with a reg pokebal, full hp */}
+				</Grid>
+				<Grid xs={5}>
 					<Typography variant="body1">{p.growth_rate.name}</Typography>
-				</div>
-			</div>
+				</Grid>
+			</Grid>
 		</Container>
 	)
 }
