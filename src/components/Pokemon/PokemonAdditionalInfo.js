958387b3b628454e9456d2c48df3779cf6a7b6eb
Anna Baker <anna.baker.design@gmail.com>
2023-03-14 01:14:00 -0400

    added to additional info section

--- src/components/Pokemon/PokemonAdditionalInfo.js
+++ src/components/Pokemon/PokemonAdditionalInfo.js
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@mui/material";
+import { Chip, Container, Typography } from "@mui/material";
 import { FemaleSharp, MaleSharp } from "@mui/icons-material";
 import { calcMeasurement } from "../../utilities/utilities";
 
@@ -10,36 +10,77 @@ export default function PokemonAdditionalInfo (props) {
 	const genderless = female < 0 || male < 0 ? true : false;
 
 	return (
-		<Container id="PokemonAdditionalInfo" className="grid">
+		<Container id="PokemonAdditionalInfo">
 			<Typography variant="h2">Additional Info</Typography>
 
-			{!genderless ? (
-				<div className="col gender grid">
-					<Typography variant="h3">Gender</Typography>
-					<div className="col">
-						<div className="pie-chart" style={{ "--male-percentage": male * 100 }}></div>
+			<div className="row grid">
+				{!genderless ? (
+					<div className="col gender grid">
+						<Typography variant="h3">Gender</Typography>
+						<div className="col">
+							<div className="pie-chart" style={{ "--male-percentage": male * 100 }}></div>
+						</div>
+						<div className="col">
+							<Typography variant="body1"><FemaleSharp sx={{ fontSize: '1em' }} /> {female * 100}%</Typography>
+							<Typography variant="body1"><MaleSharp sx={{ fontSize: '1em' }} /> {male * 100}%</Typography>
+						</div>
 					</div>
+				) : (
 					<div className="col">
-						<Typography variant="body1"><FemaleSharp sx={{ fontSize: '1em' }} /> {female * 100}%</Typography>
-						<Typography variant="body1"><MaleSharp sx={{ fontSize: '1em' }} /> {male * 100}%</Typography>
+						<Typography variant="body1">Genderless</Typography>
 					</div>
-				</div>
-			) : (
-				<div className="col">
-					<Typography variant="body1">Genderless</Typography>
-				</div>
-			)}
+				)}
 
-			<div className="col">
-				<Typography variant="h3">Height</Typography>
-				<Typography variant="body1">{calcMeasurement(p.height, "in")}</Typography>
-				<Typography variant="body1">{calcMeasurement(p.height, "cm")}</Typography>
+				<div className="col">
+					<Typography variant="h3">Height</Typography>
+					<Typography variant="body1">{calcMeasurement(p.height, "in")}</Typography>
+					<Typography variant="body1">{calcMeasurement(p.height, "cm")}</Typography>
+				</div>
+
+				<div className="col">
+					<Typography variant="h3">Weight</Typography>
+					<Typography variant="body1">{calcMeasurement(p.weight, "lbs")}</Typography>
+					<Typography variant="body1">{calcMeasurement(p.weight, "kg")}</Typography>
+				</div>
 			</div>
 
-			<div className="col">
-				<Typography variant="h3">Weight</Typography>
-				<Typography variant="body1">{calcMeasurement(p.weight, "lbs")}</Typography>
-				<Typography variant="body1">{calcMeasurement(p.weight, "kg")}</Typography>
+			<div className="row">
+				<div className="col">
+					<Typography variant="h3">EV Yield</Typography>
+					{p.stats.filter(f => f.effort > 0).map(m => (
+						<Chip size="small" label={`${m.effort} ${m.stat.name}`} />
+					))}
+				</div>
+			</div>
+
+			<div className="row">
+				<div className="col">
+					<Typography component="h4" variant="h6">Catch Rate</Typography>
+				</div>
+				<div className="col">
+					{/* // TODO: need to figure out math for capture rate with a reg pokebal, full hp */}
+					<Typography variant="body1">{p.capture_rate}</Typography>
+				</div>
+				<div className="col">
+					<Typography component="h4" variant="h6">Base Friendship</Typography>
+				</div>
+				<div className="col">
+					<Typography variant="body1"></Typography>
+				</div>
+				<div className="col">
+					<Typography component="h4" variant="h6">Base Experience</Typography>
+				</div>
+				<div className="col">
+					{/* // TODO: need to figure out math for capture rate with a reg pokebal, full hp */}
+					<Typography variant="body1">{p.base_experience}</Typography>
+				</div>
+				<div className="col">
+					<Typography component="h4" variant="h6">Growth Rate</Typography>
+				</div>
+				<div className="col">
+					{/* // TODO: need to figure out math for capture rate with a reg pokebal, full hp */}
+					<Typography variant="body1">{p.growth_rate.name}</Typography>
+				</div>
 			</div>
 		</Container>
 	)
