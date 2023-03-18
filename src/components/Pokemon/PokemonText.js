a0acb2e22c4f7a6360f09c66ec233aff9aa50cbc
Anna Baker <anna.baker.design@gmail.com>
2023-03-18 17:11:13 -0400

    flavor text logic

--- src/components/Pokemon/PokemonText.js
+++ src/components/Pokemon/PokemonText.js
@@ -1,20 +1,67 @@
 import { useState } from "react";
-import { Container, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Container, Tab, Tabs, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import generations from "../../data/generations.json";
-import { lowerCaseDashes, lowerCaseNoSpaces, romanize } from "../../utilities/utilities";
-import { Box } from "@mui/system";
+import { formatDexId, lowerCaseNoSpaces, romanize } from "../../utilities/utilities";
 
 const PokemonText = props => {
 	const p = props;
-	
-	const [value, setValue] = useState(0);
-
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
+	const flavorTextFilter = p.flavor_text_entries.filter(f => f.language.name === "en");
+	const pokedexNumbers = p.pokedex_numbers;
+	const flavorTextEntries = {
+		gen_i: [
+			{
+				game: "red",
+				kanto: {
+					num: pokedexNumbers.filter(f => f.pokedex.name === "kanto").map(m => m.entry_number)[0],
+					text: flavorTextFilter.filter(f => f.version.name === "red").map(m => m.flavor_text)[0]
+				}
+			},
+			{
+				game: "blue",
+				kanto: {
+					num: pokedexNumbers.filter(f => f.pokedex.name === "kanto").map(m => m.entry_number)[0],
+					text: flavorTextFilter.filter(f => f.version.name === "blue").map(m => m.flavor_text)[0]
+				}
+			},
+			{
+				game: "yellow",
+				kanto: {
+					num: pokedexNumbers.filter(f => f.pokedex.name === "kanto").map(m => m.entry_number)[0],
+					text: flavorTextFilter.filter(f => f.version.name === "yellow").map(m => m.flavor_text)[0]
+				}
+			},
+		],
+		gen_v: [
+			{
+				game: "black",
+				original_unova: {
+					num: pokedexNumbers.filter(f => f.pokedex.name === "original-unova").map(m => m.entry_number)[0],
+					text: flavorTextFilter.filter(f => f.version.name === "black").map(m => m.flavor_text)[0]
+				}
+			},
+			{
+				game: "white",
+				original_unova: {
+					num: pokedexNumbers.filter(f => f.pokedex.name === "original-unova").map(m => m.entry_number)[0],
+					text: flavorTextFilter.filter(f => f.version.name === "white").map(m => m.flavor_text)[0]
+				}
+			},
+		],
 	}
 
-	
+	const [value, setValue] = useState(0);
+
+	const handleChange = (event, newValue) => { setValue(newValue); }
+
+	const filterVersions = (flavorText, games) => {
+		let filter = [];
+		
+		for (let i = 0; i < games.length; i++) {
+			filter.push(flavorText.filter(f => f.version.name === games[i]))
+		};
+		
+		return filter[0].length > 0 ? true : false;
+	}
 
 	return (
 		<Container id="PokemonText" sx={{ mb: 5 }}>
@@ -26,24 +73,73 @@ const PokemonText = props => {
 					scrollButtons="auto"
 					aria-label="game flavor text sorted by generation"
 				>
-					{generations.results.map(g => (
-						<Tab key={g.id} label={g.shortName} id={g.id} aria-controls={`panel${g.shortName.replace(/\s/g, '')}`} />
-					))}
+					{filterVersions(flavorTextFilter, ["red", "blue", "yellow"]) ? (
+						<Tab label="Gen I" id="genTabI" aria-controls="panelGenI" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["gold", "silver", "crystal"]) ? (
+						<Tab label="Gen II" id="genTabII" aria-controls="panelGenII" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["ruby", "sapphire", "emerald", "firered", "leafgreen", "colosseum", "xd"]) ? (
+						<Tab label="Gen III" id="genTabIII" aria-controls="panelGenIII" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["diamond", "pearl", "platinum", "heartgold", "soulsilver"]) ? (
+						<Tab label="Gen IV" id="genTabIV" aria-controls="panelGenIV" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["black", "white", "black-2", "white-2"]) ? (
+						<Tab label="Gen V" id="genTabV" aria-controls="panelGenV" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["x", "y", "omega-ruby", "alpha-sapphire"]) ? (
+						<Tab label="Gen VI" id="genTabVI" aria-controls="panelGenVI" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["sun", "moon", "ultra-sun", "ultra-moon", "lets-go-pikachu", "lets-go-eevee"]) ? (
+						<Tab label="Gen VII" id="genTabVII" aria-controls="panelGenVII" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["sword", "shield", "the-isle-of-armor", "the-crown-tundra", "brilliant-diamond", "shining-pearl", "legends-arceus"]) ? (
+						<Tab label="Gen VIII" id="genTabVIII" aria-controls="panelGenVIII" />
+					) : null}
+
+					{filterVersions(flavorTextFilter, ["scarlet", "violet", "the-teal-mask", "the-indigo-disk"]) ? (
+						<Tab label="Gen IX" id="genTabIX" aria-controls="panelGenIX" />
+					) : null}
 
 				</Tabs>
 			</Box>
-			{generations.results.map((g, index) => (
-				<GenerationPanel key={index} value={value} index={index} generation={g}>
-					{g.version.map(v => (
-						<FlavorText
-							key={v.name}
-							game={v.name}
-							text={p.flavor_text_entries.filter(f => f.version.name === lowerCaseDashes(v.name) && f.language.name === "en")}
-							num={v.pokedex.map(pd => p.pokedex_numbers.filter(f => f.pokedex.name === lowerCaseDashes(pd)))}
-						/>
+			{filterVersions(flavorTextFilter, ["red", "blue", "yellow"]) ? (
+				<GenerationPanel value={0} index={0} gen="I">
+					{flavorTextEntries.gen_i.map(m => (
+						m.kanto.text ? (
+							<FlavorText
+								key={m.game}
+								game={m.game}
+								num={m.kanto.num}
+								text={m.kanto.text}
+							/>
+						) : null
 					))}
 				</GenerationPanel>
-			))}
+			) : null}
+
+			{filterVersions(flavorTextFilter, ["black", "white", "black-2", "white-2"]) ? (
+				<GenerationPanel value={4} index={4} gen="V">
+					{flavorTextEntries.gen_v.map(m => (
+						m.original_unova.text ? (
+							<FlavorText
+								key={m.game}
+								game={m.game}
+								num={m.original_unova.num}
+								text={m.original_unova.text}
+							/>
+						) : null
+					))}
+				</GenerationPanel>
+			) : null}
 		</Container>
 	)
 }
@@ -51,19 +147,16 @@ const PokemonText = props => {
 
 
 const GenerationPanel = props => {
-	const romanNum = romanize(props.index + 1);
-
 	return (
 		<Container
 			role="tabpanel"
 			hidden={props.value !== props.index}
-			id={`panelGen${romanNum}`}
-			aria-labelledby={`genTab${romanNum}`}
+			id={`panelGen${props.gen}`}
+			aria-labelledby={`genTab${props.gen}`}
 		>
 			{props.value === props.index && (
 				props.children
 			)}
-			
 		</Container>
 	)
 }
@@ -72,15 +165,18 @@ const GenerationPanel = props => {
 
 const FlavorText = props => {
 	const game = props.game;
-	const num = props.num[0][0] ? props.num[0][0].entry_number : null;
-	const text = props.text[0] ? props.text[0].flavor_text : null;
+	const num = props.num ? props.num : null;
+	const text = props.text ? props.text : null;
 
 	if (text) {
 		return (
-			<Grid>
-				<Typography component="h3" variant="h6" className={`game__${lowerCaseNoSpaces(game)}`}>{game}</Typography>
+			<Grid mt={2}>
+				<Typography component="h3" variant="h6" className={`game__${game}`} sx={{ textTransform: "capitalize" }}>{game}</Typography>
 				<Typography variant="body1">
-					<Typography component="span" variant="caption">{num}</Typography>
+					<Typography component="span" variant="caption" mr={0.5}>
+						<Typography component="span" variant="caption" fontSize="0.7em" mr={0.3}>NO.</Typography>
+						{formatDexId(num)}
+					</Typography>
 					{text.replace(/\s/g, " ")}
 				</Typography>
 			</Grid>
