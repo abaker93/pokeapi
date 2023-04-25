a843593132712ff91b90e0c6860d5f6203fd1e48
Anna Baker <anna.baker.design@gmail.com>
2023-04-25 10:34:21 -0400

    sprite table

--- src/components/Pokemon/PokemonSprites.js
+++ src/components/Pokemon/PokemonSprites.js
@@ -1,5 +1,7 @@
-import { Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Container, Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import ArrowForwardSharp from "@mui/icons-material/ArrowForwardSharp";
 import React from "react";
+import { formatDexId } from "../../utilities/utilities";
 
 const PokemonSprites = props => {
 	const pokeName = props.names.filter(f => f.language.name === "en").map(m => m.name)[0];
@@ -11,65 +13,57 @@ const PokemonSprites = props => {
 			id: 1,
 			name: "I",
 			gray: true,
-			normal: props.sprites.versions["generation-i"]["red-blue"].front_gray ? true : false,
-			shiny: null,
 			sprites: props.sprites.versions["generation-i"]["red-blue"],
+			bool: props.sprites.versions["generation-i"]["red-blue"].front_gray ? true : false,
 		},
 		{
 			id: 2,
 			name: "II",
 			gray: false,
-			normal: props.sprites.versions["generation-ii"].gold.front_default ? true : false,
-			shiny: props.sprites.versions["generation-ii"].gold.front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-ii"].gold,
+			bool: props.sprites.versions["generation-ii"].gold.front_default ? true : false,
 		},
 		{
 			id: 3,
 			name: "III",
 			gray: false,
-			normal: props.sprites.versions["generation-iii"]["ruby-sapphire"].front_default ? true : false,
-			shiny: props.sprites.versions["generation-iii"]["ruby-sapphire"].front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-iii"]["ruby-sapphire"],
+			bool: props.sprites.versions["generation-iii"]["ruby-sapphire"].front_default ? true : false,
 		},
 		{
 			id: 4,
 			name: "IV",
 			gray: false,
-			normal: props.sprites.versions["generation-iv"]["diamond-pearl"].front_default ? true : false,
-			shiny: props.sprites.versions["generation-iv"]["diamond-pearl"].front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-iv"]["diamond-pearl"],
+			bool: props.sprites.versions["generation-iv"]["diamond-pearl"].front_default ? true : false,
 		},
 		{
 			id: 5,
 			name: "V",
 			gray: false,
-			normal: props.sprites.versions["generation-v"]["black-white"].front_default ? true : false,
-			shiny: props.sprites.versions["generation-v"]["black-white"].front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-v"]["black-white"],
+			bool: props.sprites.versions["generation-v"]["black-white"].front_default ? true : false,
 		},
 		{
 			id: 6,
 			name: "VI",
 			gray: false,
-			normal: props.sprites.versions["generation-vi"]["x-y"].front_default ? true : false,
-			shiny: props.sprites.versions["generation-vi"]["x-y"].front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-vi"]["x-y"],
+			bool: props.sprites.versions["generation-vi"]["x-y"].front_default ? true : false,
 		},
 		{
 			id: 7,
 			name: "VII",
 			gray: false,
-			normal: props.sprites.versions["generation-vii"].icons.front_default ? true : false,
-			shiny: props.sprites.versions["generation-vii"].icons.front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-vii"].icons,
+			bool: props.sprites.versions["generation-vii"].icons.front_default ? true : false,
 		},
 		{
 			id: 8,
 			name: "VIII",
 			gray: false,
-			normal: props.sprites.versions["generation-viii"].icons.front_default ? true : false,
-			shiny: props.sprites.versions["generation-viii"].icons.front_shiny ? true : false,
 			sprites: props.sprites.versions["generation-viii"].icons,
+			bool: props.sprites.versions["generation-viii"].icons.front_default ? true : false,
 		},
 	]
 
@@ -83,7 +77,7 @@ const PokemonSprites = props => {
 						<TableRow>
 							<TableCell>Generation</TableCell>
 							{sprites.map(m => (
-								m.normal ? (<TableCell key={m.id}>{m.name}</TableCell>) : null
+								m.bool ? (<TableCell key={m.id}>{m.name}</TableCell>) : null
 							))}
 						</TableRow>
 					</TableHead>
@@ -91,7 +85,7 @@ const PokemonSprites = props => {
 						<TableRow>
 							<TableCell>Normal</TableCell>
 							{sprites.map(m => (
-								m.normal ? (
+								m.bool ? (
 									m.gray ? (
 										<TableCell key={m.id}>
 											{m.sprites.front_gray ? (
@@ -131,11 +125,34 @@ const PokemonSprites = props => {
 							))}
 						</TableRow>
 						<TableRow>
-							
+							<TableCell>Shiny</TableCell>
+							{sprites.map(m => (
+								m.bool ? (
+									<TableCell key={m.id}>
+										{m.sprites.front_shiny ? (
+											<img
+												src={m.sprites.front_shiny}
+												alt={`${pokeName} generation ${m.name} shiny front sprite`}
+												className="sprite sprite-normal sprite-front sprite-shiny"
+											/>
+										) : <>&mdash;</>}
+										{m.sprites.back_shiny ? (
+											<img
+												src={m.sprites.back_shiny}
+												alt={`${pokeName} generation ${m.name} shiny back sprite`}
+												className="sprite sprite-normal sprite-back sprite-shiny"
+											/>
+										) : null}
+									</TableCell>
+								) : null
+							))}
 						</TableRow>
 					</TableBody>
 				</Table>
 			</TableContainer>
+			<Link href={`/pokemon/${formatDexId(props.id)}/sprites`} underline="hover">
+				<Typography variant="h6">See all {pokeName} sprites <ArrowForwardSharp /></Typography>
+			</Link>
 		</Container>
 	)
 }
